00ff3a4a4e59763cf04301983296f41f3339e4a0
Kevin Ossa <kevin.ossa@utp.edu.co>
2024-06-03 23:21:29 -0500

    Algoritmo sin FFT completo, errores en algoritmo con FFT

--- prime_digit_in_e_fft.py
+++ prime_digit_in_e_fft.py
@@ -1,14 +1,11 @@
 from sympy import isprime
 import numpy as np
-import decimal
-
-def is_ten_digit(n):
-	return len(str(n)) == 10
 
 def fft_multiplication(a, b):
 	n = len(a) + len(b) - 1
-	a = np.fft.fft(a, n)
-	b = np.fft.fft(b, n)
+	n_transform = 2 ** int(np.ceil(np.log2(n)))
+	a = np.fft.fft(a, n_transform)
+	b = np.fft.fft(b, n_transform)
 	c = np.fft.ifft(a * b)
 	return np.rint(c).astype(int)[:n]
 
@@ -16,29 +13,44 @@ def factorial(n):
 	if n == 0 or n == 1:
 		return [1]
 	else:
-		fact = np.arange(1, n+1)
+		fact = [1]
 		for i in range(2, n):
 			fact = fft_multiplication(fact, np.array([i]))
+		print(f"{fact}")
 		return fact
 	
 def compute_e(n):
-	decimal.getcontext().prec = n + 1
-	e = 0
-	fact = factorial(n)
-	for i in range(n):
-		e += 1 / fact[i]
-	return e
+	digits = [2]
+	factorials = [1]
+	for i in range(1, n):
+		factorials.append(factorial(i))
+	for k in range(1, n):
+		term = [1]
+		factorial_k = factorials[k]
+		
+		for i in range(len(term)):
+			if i < len(factorial_k) and factorial_k[i] != 0:
+				term[i] //= factorial_k[i]
+			else:
+				term[i] = 0
+		
+		if len(term) > len(digits):
+			digits.extend([0] * (len(term) - len(digits)))
+		for i in range(len(term)):
+			digits[i] += term[i]
+
+	return digits if len(digits) <= n else digits[:n]
 	
 def tenth_prime_digit_in_e(e_number):
-	e = str(e_number)[2:]
-	for i in range(len(e)):
+	e = ''.join(map(str, e_number))
+	for i in range(len(e) - 9):
 		number = int(e[i:i+10])
 		if isprime(number):
 			return number
 	return None
 
 if __name__ == "__main__":
-	n = 200
+	n = 20
 	e = compute_e(n)
 	print(e)
 	print(tenth_prime_digit_in_e(e))
